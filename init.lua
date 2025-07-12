c899e52d81342898e3792c35785d2a9ae221a163
Surya G <surya.gaddipati@gmail.com>
2025-07-12 15:46:11 -0500

    markdown

--- init.lua
+++ init.lua
@@ -314,6 +314,46 @@ require('lazy').setup({
     end,
   },
 
+  -- Markdown preview in browser
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+    keys = {
+      { '<leader>p', '<cmd>MarkdownPreviewToggle<cr>', desc = 'Markdown preview' },
+    },
+    config = function()
+      vim.g.mkdp_auto_start = 0
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_command_for_global = 0
+      vim.g.mkdp_open_to_the_world = 0
+      vim.g.mkdp_open_ip = ''
+      vim.g.mkdp_browser = ''
+      vim.g.mkdp_echo_preview_url = 0
+      vim.g.mkdp_browserfunc = ''
+      vim.g.mkdp_preview_options = {
+        mkit = {},
+        katex = {},
+        uml = {},
+        maid = {},
+        disable_sync_scroll = 0,
+        sync_scroll_type = 'middle',
+        hide_yaml_meta = 1,
+        sequence_diagrams = {},
+        flowchart_diagrams = {},
+        content_editable = false,
+        disable_filename = 0
+      }
+      vim.g.mkdp_markdown_css = ''
+      vim.g.mkdp_highlight_css = ''
+      vim.g.mkdp_port = ''
+      vim.g.mkdp_page_title = '「${name}」'
+      vim.g.mkdp_filetypes = {'markdown'}
+    end,
+  },
+
   -- Claude Code integration (official Coder plugin)
   {
     'coder/claudecode.nvim',
