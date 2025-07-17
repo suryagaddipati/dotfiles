d2215a5ae7ebfbdad455b3e35246f69056868934
Surya G <surya.gaddipati@gmail.com>
2025-07-17 08:44:44 -0500

    hightlights

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,12 +1,15 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  event = { 'BufReadPost', 'BufNewFile' },
+  event = { 'BufReadPost', 'BufNewFile', 'BufWinEnter' },
   config = function()
     require('nvim-treesitter.configs').setup({
       ensure_installed = { 'lua', 'python', 'javascript', 'typescript', 'bash', 'json', 'yaml' },
       auto_install = true,
-      highlight = { enable = true },
+      highlight = { 
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
       indent = { enable = true },
     })
   end,
