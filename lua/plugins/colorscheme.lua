09f003e4eb7c8db12c7cfe7075d2bc62eac1c066
Surya G <surya.gaddipati@gmail.com>
2025-07-17 09:29:53 -0500

    focus on split in action in nvim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,5 +6,14 @@ return {
     vim.g.gruvbox_contrast_dark = 'hard'
     vim.g.gruvbox_bold = 1
     vim.cmd.colorscheme('gruvbox')
+    
+    -- Configure window focus highlighting
+    vim.api.nvim_set_hl(0, 'NormalNC', { bg = '#1d2021', fg = '#928374' })
+    vim.api.nvim_set_hl(0, 'Normal', { bg = '#282828', fg = '#ebdbb2' })
+    vim.api.nvim_set_hl(0, 'VertSplit', { fg = '#504945', bg = '#1d2021' })
+    vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#83a598', bg = 'NONE' })
+    
+    -- Make focused window border more prominent
+    vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#83a598', bg = 'NONE' })
   end,
 }
---
+++ lua/plugins/focus.lua
@@ -0,0 +1,36 @@
+return {
+  'nvim-focus/focus.nvim',
+  version = '*',
+  config = function()
+    require('focus').setup({
+      enable = true,
+      commands = true,
+      autoresize = {
+        enable = true,
+        width = 0,
+        height = 0,
+        minwidth = 0,
+        minheight = 0,
+        height_quickfix = 10,
+      },
+      split = {
+        bufnew = false,
+        tmux = false,
+      },
+      ui = {
+        number = false,
+        relativenumber = false,
+        hybridnumber = false,
+        absolutenumber_unfocussed = false,
+        cursorline = true,
+        cursorcolumn = false,
+        colorcolumn = {
+          enable = false,
+          list = '+1',
+        },
+        signcolumn = true,
+        winhighlight = true,
+      }
+    })
+  end,
+}
