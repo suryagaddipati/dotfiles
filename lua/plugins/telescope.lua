27cb19e0745b54a66af6cb72f78f5ced9f9c774e
Surya G <meowlicious99@gmail.com>
2025-07-19 15:15:29 -0500

    change keybindigs because g is is for git now

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,9 @@
 return {
   'nvim-telescope/telescope.nvim',
-  cmd = 'Telescope',
   dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
-    { '<leader>f', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
-    { '<leader>g', '<cmd>Telescope live_grep<cr>', desc = 'Grep' },
+    { '<leader>f', '<cmd>Telescope live_grep<cr>', desc = 'Live grep' },
+    { '<leader>p', '<cmd>Telescope find_files<cr>', desc = 'Find files' },
     { '<leader>b', '<cmd>Telescope buffers<cr>', desc = 'Buffers' },
     { '<leader>hr', '<cmd>Telescope oldfiles<cr>', desc = 'Recent files' },
   },
