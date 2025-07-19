97553cf720cb0fc39e1ca20c0ef060d6efb203fc
Surya G <meowlicious99@gmail.com>
2025-07-19 15:14:57 -0500

    set size for plugin

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,13 +1,16 @@
 return {
   'nvim-tree/nvim-tree.lua',
-  cmd = { 'NvimTreeToggle', 'NvimTreeFocus' },
   keys = {
     { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle tree' },
   },
   config = function()
     require('nvim-tree').setup({
       hijack_netrw = true,
-      view = { width = 30 },
+      view = { 
+        width = 30,
+        adaptive_size = false,
+        preserve_window_proportions = false,
+      },
       renderer = { group_empty = true },
       filters = { dotfiles = false },
       git = { enable = true },
