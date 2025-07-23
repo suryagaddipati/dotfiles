a4b590ce320b18a30e6098dd447f192513d39847
Surya G <meowlicious99@gmail.com>
2025-07-23 14:16:58 -0500

    docs: update CLAUDE.md with corrected keybindings and fix toggleterm default
    - Fix Neovim leader key documentation (Space, not comma)
    - Update keybinding examples throughout documentation
    - Correct toggleterm default behavior to use floating terminal
    - Align documentation with actual configuration

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,7 @@ return {
   version = '*',
   keys = {
     { '<C-\\>', '<cmd>ToggleTerm<cr>', desc = 'Toggle terminal' },
-    { '<leader>tt', '<cmd>ToggleTerm<cr>', desc = 'Toggle terminal' },
+    { '<leader>tt', '<cmd>1ToggleTerm direction=float<cr>', desc = 'Toggle terminal' },
     { '<leader>tf', '<cmd>ToggleTerm direction=float<cr>', desc = 'Float terminal' },
     { '<leader>th', '<cmd>ToggleTerm direction=horizontal<cr>', desc = 'Horizontal terminal' },
     { '<leader>tv', '<cmd>ToggleTerm direction=vertical<cr>', desc = 'Vertical terminal' },
