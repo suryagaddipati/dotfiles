a7bf7c131bbe2e3b36ac85379fec6ed95efb3fb0
Surya G <meowlicious99@gmail.com>
2025-07-28 08:26:14 -0500

    refactor: replace telescope and maximizer plugins with snacks.nvim

--- lua/plugins/maximizer.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'szw/vim-maximizer',
-  keys = {
-    { '<leader>z', '<cmd>MaximizerToggle<cr>', desc = 'Toggle maximize split' },
-  },
-  config = function()
-    vim.g.maximizer_set_default_mapping = 0
-  end,
-}
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,60 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  keys = {
+    -- Picker functionality (replaces telescope)
+    { '<leader>e', function() Snacks.picker.grep() end, desc = 'Live grep' },
+    { '<leader>f', function() Snacks.picker.files() end, desc = 'Find files' },
+    { '<leader>b', function() Snacks.picker.buffers() end, desc = 'Buffers' },
+    { '<leader>hr', function() Snacks.picker.recent() end, desc = 'Recent files' },
+    { '<leader>r', function() Snacks.picker.registers() end, desc = 'Registers' },
+    { '<leader>:', function() Snacks.picker.command_history() end, desc = 'Command history' },
+    { '<leader>;', function() Snacks.picker.commands() end, desc = 'Commands' },
+    
+    -- Zen/Focus functionality (replaces maximizer + focus)
+    { '<leader>z', function() Snacks.zen.zoom() end, desc = 'Toggle maximize split' },
+  },
+  opts = {
+    -- Picker configuration
+    picker = {
+      sources = {
+        files = { hidden = true },
+        grep = { hidden = true },
+      },
+      win = {
+        input = {
+          keys = {
+            ['<C-u>'] = { 'scroll_up', mode = { 'i', 'n' } },
+            ['<C-d>'] = { 'scroll_down', mode = { 'i', 'n' } },
+          },
+        },
+      },
+    },
+    
+    -- Zen mode configuration
+    zen = {
+      toggles = {
+        git_signs = true,
+        mini_diff_signs = true,
+        diagnostics = true,
+        inlay_hints = true,
+      },
+      zoom = {
+        toggles = {
+          git_signs = false,
+          mini_diff_signs = false,
+          diagnostics = false,
+          inlay_hints = false,
+        },
+      },
+    },
+    
+    -- Enable additional snacks modules
+    bigfile = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
