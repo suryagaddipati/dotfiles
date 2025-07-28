6b75fced9dba10a4c082a2b44a3e3ca4bf3b4281
Surya G <surya.gaddipati@gmail.com>
2025-07-27 19:49:40 -0500

    feat: enable statusline with file path and cursor position

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -43,9 +43,12 @@ vim.opt.guicursor = {
 }
 
 -- Better visual feedback
-vim.opt.laststatus = 0  -- No status line
+vim.opt.laststatus = 2  -- Always show status line
 vim.opt.showcmd = true
-vim.opt.showmode = false  -- Let lualine handle mode display
+vim.opt.showmode = false  -- Status line will show mode
+
+-- Custom statusline with file path
+vim.opt.statusline = '%f %m%r%h%w%=%l,%c %p%%'
 
 vim.opt.mouse = 'a'
 vim.opt.clipboard = 'unnamedplus'
