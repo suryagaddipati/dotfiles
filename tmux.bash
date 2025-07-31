4b1320cc68edbe6c581f08dd202bdf3040497579
Surya G <meowlicious99@gmail.com>
2025-07-31 09:18:11 -0500

    refactor: simplify dotfiles scripts by removing verbose output and comments

--- tmux.bash
+++ tmux.bash
@@ -1,25 +1,14 @@
-# Brett Terpstra 2014
-# <http://brettterpstra.com>
-#
-# tmux wrapper
-# 	tm session-name [window-name]
-# Names can be partial from the beginning and first match will connect.
-# If no match is found a new session will be created.
-# If there's a second argument, it will be used to attach directly to a
-# window in the session, or to name the first window in a new session.
 tm() {
 	local attach window
 	if [ -n $1 ]; then
 		attach=""
 
-		tmux has-session -t $1 > /dev/null
-		if [ $? -eq 0 ]; then
+		if tmux has-session -t $1 2>/dev/null; then
 			attach=$1
 			shift
 		else
-			for session in `tmux ls|awk -F ":" '{ print $1 }'`;do
-				if [[ $session =~ ^$1  ]]; then
-					echo "Matched session: $session"
+			for session in `tmux ls 2>/dev/null|awk -F ":" '{ print $1 }'`;do
+				if [[ $session =~ ^$1 ]]; then
 					attach=$session
 					shift
 					break
@@ -31,12 +20,10 @@ tm() {
 			if [ $# -eq 1 ]; then
 				for win in `tmux list-windows -t $attach|sed -E 's/^[0-9]+: //'|sed -E 's/[*-].*//'`;do
 					if [[ $win =~ ^$1 ]]; then
-						echo "Matched window: $window"
 						window=$win
 						break
 					fi
 				done
-
 				tmux attach -t $attach:$window
 			else
 				tmux attach -t $attach
@@ -45,13 +32,12 @@ tm() {
 			if [ $# -gt 1 ]; then
 				attach=$1
 				shift
-				tmux new $SHELL  -s $attach -n $1  \; set default-shell $SHELL
+				tmux new -s $attach -n $1
 			else
-				echo "Attempting to create $1"
-				tmux new  -s $1   \; set default-shell $SHELL 
+				tmux new -s $1
 			fi
 		fi
 	else
-		tmux new $SHELL \; set default-shell $SHELL
+		tmux new
 	fi
 }
