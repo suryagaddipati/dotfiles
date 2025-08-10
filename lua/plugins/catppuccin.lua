02c1f1451e17931b6868c43b23a90149d7cb39a3
Surya G <surya.gaddipati@gmail.com>
2025-08-10 08:41:53 -0500

    refactor: move colorscheme plugins back to main plugins directory
    Simplified plugin structure by moving catppuccin and rose colorschemes
    from lua/plugins/colors/ back to lua/plugins/ for better organization

---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,31 @@
+return {
+  'catppuccin/nvim',
+  name = 'catppuccin',
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require('catppuccin').setup({
+      flavour = "auto", -- latte, frappe, macchiato, mocha
+      background = {    -- :h background
+        light = "latte",
+        dark = "mocha",
+      },
+      transparent_background = false,
+      default_integrations = true,
+      auto_integrations = false,
+      color_overrides = {
+        mocha = {
+          base = "#000000",
+          mantle = "#000000",
+          crust = "#000000",
+        },
+      },
+      dim_inactive = {
+        enabled = true,
+        shade = "dark",
+        percentage = 1.9,
+      },
+    })
+    vim.cmd.colorscheme('catppuccin')
+  end,
+}
--- lua/plugins/colors/catppuccin.lua
+++
@@ -1,6 +0,0 @@
-return {
-  'catppuccin/nvim',
-  name = 'catppuccin',
-  lazy = false,
-  priority = 1000,
-}
