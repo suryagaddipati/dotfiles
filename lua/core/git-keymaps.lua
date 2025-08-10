73eaedc996aa8532e4968ab4fca765c306da9b0c
Surya G <surya.gaddipati@gmail.com>
2025-08-10 10:28:10 -0500

    Revert "fix: update git-keymaps to use Snacks.notifier and modern nvim APIs"
    This reverts commit e91287663d7364f77e507d3207b5036edfb6af74.

--- lua/core/git-keymaps.lua
+++ lua/core/git-keymaps.lua
@@ -13,19 +13,11 @@ local function run_git_command(command, message, success_msg, error_msg)
       Snacks.notifier.hide(note_id)
       if result.code == 0 then
         if success_msg then
-          Snacks.notifier.notify(success_msg, "info", {
-            title = "Git",
-            position = "bottom_right",
-            timeout = 3000,
-          })
+          vim.notify(success_msg, vim.log.levels.INFO)
         end
       else
         local error_text = error_msg or "Git command failed"
-        Snacks.notifier.notify(error_text .. ": " .. (result.stderr or ""), "error", {
-          title = "Git",
-          position = "bottom_right",
-          timeout = 5000,
-        })
+        vim.notify(error_text .. ": " .. (result.stderr or ""), vim.log.levels.ERROR)
       end
     end)
   end)
@@ -117,9 +109,9 @@ keymap('n', '<leader>gwl', function()
     local buf = vim.api.nvim_create_buf(false, true)
     local lines = vim.split(output, '\n')
     vim.api.nvim_buf_set_lines(buf, 0, -1, false, lines)
-    vim.bo[buf].buftype = 'nofile'
-    vim.bo[buf].bufhidden = 'wipe'
-    vim.bo[buf].filetype = 'gitworktree'
+    vim.api.nvim_buf_set_option(buf, 'buftype', 'nofile')
+    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'wipe')
+    vim.api.nvim_buf_set_option(buf, 'filetype', 'gitworktree')
 
     -- Open in a split
     vim.cmd('split')
@@ -136,9 +128,9 @@ keymap('n', '<leader>gwl', function()
         local worktree_name = line:match('^(%S+)')
         if worktree_name and worktree_name ~= '' then
           vim.cmd('q') -- Close the list window
-          local wt_output = vim.fn.system('git wt ' .. worktree_name)
+          local output = vim.fn.system('git wt ' .. worktree_name)
           if vim.v.shell_error == 0 then
-            local cd_cmd = wt_output:match('cd (.+)')
+            local cd_cmd = output:match('cd (.+)')
             if cd_cmd then
               vim.cmd('cd ' .. cd_cmd)
               vim.notify('Switched to worktree: ' .. worktree_name, vim.log.levels.INFO)
